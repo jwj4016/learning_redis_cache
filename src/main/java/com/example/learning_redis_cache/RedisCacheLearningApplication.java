1981cca414afc7138603f8a544312d0188098856
J <jwj4016@gmail.com>
2023-03-31 17:23:14 +0900

    Initial commit

---
+++ src/main/java/com/example/learning_redis_cache/RedisCacheLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_redis_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCacheLearningApplication.class, args);
+    }
+
+}
